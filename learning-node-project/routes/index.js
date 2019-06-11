4bd64edbde35e44afdcbc9a073f1365a23337b3b
gegorov <gegorov@yahoo.com>
2019-06-11 13:35:05 +0300

    refactor routes to use controller

--- learning-node-project/routes/index.js
+++ learning-node-project/routes/index.js
@@ -1,23 +1,8 @@
 const express = require('express');
+
 const router = express.Router();
-
+const storeController = require('../controllers/storeController');
 // Do work here
-router.get('/', (req, res) => {
-
-  const wes = {
-    name: 'gg',
-    cool: true,
-  }
-  // res.send('Hey! It works!');
-  // res.send('LALALALA works');
-  console.log(req.query);
-  res.json(req.query);
-});
-
-router.get('/reverse/:name', (req, res) => {
-  console.log(`======>>>>> ${typeof req.params.name}`);
-  const reverse = [...req.params.name].reverse().join('')
-  res.send(reverse);
-})
+router.get('/', storeController.homepage);
 
 module.exports = router;
