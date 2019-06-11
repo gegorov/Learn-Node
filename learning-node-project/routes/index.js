09b8ca36b582964d4ed64efd49ea9aafe42390c4
gegorov <gegorov@yahoo.com>
2019-06-11 11:29:19 +0300

    playing around with routes

--- learning-node-project/routes/index.js
+++ learning-node-project/routes/index.js
@@ -3,7 +3,21 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+
+  const wes = {
+    name: 'gg',
+    cool: true,
+  }
+  // res.send('Hey! It works!');
+  // res.send('LALALALA works');
+  console.log(req.query);
+  res.json(req.query);
 });
+
+router.get('/reverse/:name', (req, res) => {
+  console.log(`======>>>>> ${typeof req.params.name}`);
+  const reverse = [...req.params.name].reverse().join('')
+  res.send(reverse);
+})
 
 module.exports = router;
