0ed0526bc5a8de7ab7e1b0d4822dc7cee285ab67
gegorov <gegorov@yahoo.com>
2019-06-12 13:43:52 +0300

    add async createStore

--- learning-node-project/controllers/storeController.js
+++ learning-node-project/controllers/storeController.js
@@ -1,3 +1,7 @@
+const mongoose = require('mongoose');
+
+const Store = mongoose.model('Store');
+
 exports.homepage = (req, res) => {
   res.render('index');
 };
@@ -6,6 +10,9 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: 'Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = new Store(req.body);
+  await store.save();
+  console.log('Saved!');
+  res.redirect('/');
 };
